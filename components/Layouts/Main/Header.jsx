425ccf43761543e156cd3f83c6a3d61c67884288
Hari Perisetla <harinagasai.perisetla@gmail.com>
2023-04-17 11:33:29 +0530

    landing page created

--- components/Layouts/Main/Header.jsx
+++ components/Layouts/Main/Header.jsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="flex justify-between py-3 px-10 fixed bg-white w-full">
+      <Link href={"/"} className="text-3xl font-extrabold text-purple-950">
+        Multroic
+      </Link>
+      <ul className="flex space-x-3 font-bold text-xl text-purple-950">
+        <li>
+          <Link href={"/"}>About</Link>
+        </li>
+        <li>
+          <Link href={"/"}>Play</Link>
+        </li>
+        <li>
+          <Link href={"/"}>Contact</Link>
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,37 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Gaming from "../assets/img/gaming.svg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => {
   return (
     <main className="">
-      <h1>test</h1>
+      <div className="bg-purple-100 h-[80vh] pt-16 items-center">
+        <div className="container px-16 mx-auto grid h-full grid-cols-2 gap-10 items-center">
+          <div className=" space-y-3">
+            <h1 className="text-6xl font-montserrat font-extrabold leading-tight text-purple-950">
+              Discover and Play Open-Source Games
+            </h1>
+            <p className="font-medium text-xl text-purple-950 text-justify">
+              Join our community of developers and gamers to explore a variety
+              of games in your browser or download them to play on your device
+            </p>
+
+            <button className="bg-purple-950 text-white px-5 py-3 text-xl font-bold">
+              Get Started
+            </button>
+          </div>
+          <div className="relative w-full h-[60vh]">
+            <Image
+              src={Gaming}
+              alt="gaming vector image"
+              fill
+              className="object-contain object-center"
+            />
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
