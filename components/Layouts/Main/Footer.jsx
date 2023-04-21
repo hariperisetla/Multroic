040e288de188662ff9e232546679cadba70f86e6
Hari Perisetla <harinagasai.perisetla@gmail.com>
2023-04-21 10:56:51 +0530

    logo added to header and footer

--- components/Layouts/Main/Footer.jsx
+++ components/Layouts/Main/Footer.jsx
@@ -1,18 +1,71 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import { FullLogoWhiteImg } from "@/assets/img/Img";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className="text-center bg-purple-900 text-white">
-      <div>social icons here</div>
-      <div>Links here</div>
-      <div className="bg-purple-950 py-3">
-        <Link href={"/"} className="hover:underline">
-          &copy; Multroic{" "}
+      <div className="py-20 container mx-auto space-x-20 max-w-6xl grid grid-cols-4">
+        <div className=" text-left">
+          <div className="relative w-64 h-7 mb-3">
+            <Image
+              src={FullLogoWhiteImg}
+              fill
+              alt="multroic logo"
+              className="object-contain object-left"
+            />
+          </div>
+          <div className="font-medium">
+            Created by{" "}
+            <Link
+              target="_blank"
+              href={"https://hariperisetla.github.io"}
+              className="underline text-purple-100"
+            >
+              Hari Perisetla
+            </Link>
+          </div>
+        </div>
+        <div className="text-center md:text-left">
+          <ul className="capitalize space-y-3">
+            <li className="uppercase font-bold">site links</li>
+            <li>about</li>
+            <li>games</li>
+            <li>developers</li>
+            <li>contact</li>
+            <li>blog</li>
+          </ul>
+        </div>
+        <div className="text-center md:text-left">
+          <ul className="capitalize space-y-3">
+            <li className="uppercase font-bold">legal</li>
+            <li>terms</li>
+            <li>privacy policy</li>
+            <li>terms of use</li>
+            <li>license</li>
+          </ul>
+        </div>
+        <div className="text-center md:text-left">
+          <ul className="capitalize space-y-3">
+            <li className="uppercase font-bold">social</li>
+            <li>twitter</li>
+            <li>facebook</li>
+            <li>instagram</li>
+            <li>youtube</li>
+          </ul>
+        </div>
+      </div>
+      <div className="bg-purple-950 py-3 font-medium">
+        &copy; {currentYear}{" "}
+        <Link
+          href={"/"}
+          className="hover:underline uppercase font-bold underline"
+        >
+          Multroic{" "}
         </Link>
-        {currentYear}
       </div>
     </footer>
   );
--- components/Layouts/Main/Header.jsx
+++ components/Layouts/Main/Header.jsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { FullLogoImg } from "@/assets/img/Img";
 
 const Header = () => {
   return (
-    <header className="z-20 flex justify-between py-3 px-10 fixed bg-white w-full">
-      <Link href={"/"} className="text-3xl font-extrabold text-purple-950">
-        Multroic
+    <header className="z-20 flex justify-between py-3 px-10 fixed bg-white w-full shadow items-center">
+      <Link href={"/"} className="text-3xl font-extrabold  text-purple-950">
+        <div className="relative w-64 h-7 my-2">
+          <Image
+            src={FullLogoImg}
+            fill
+            alt="multroic logo"
+            className="object-contain object-center"
+          />
+        </div>
       </Link>
       <ul className="flex space-x-8 font-bold text-xl text-purple-950">
         <li>
