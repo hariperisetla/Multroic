754033c98c234e8a34fc721e4e266960f3e998d1
Hari Perisetla <harinagasai.perisetla@gmail.com>
2023-04-21 11:49:34 +0530

    mobile responsivness added and menu icon added

--- components/Layouts/Main/Header.jsx
+++ components/Layouts/Main/Header.jsx
@@ -2,21 +2,27 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { FullLogoImg } from "@/assets/img/Img";
+import { GiHamburgerMenu } from "@/assets/icons/index";
 
 const Header = () => {
   return (
-    <header className="z-20 flex justify-between py-3 px-10 fixed bg-white w-full shadow items-center">
+    <header className="z-20 flex justify-between py-3 px-5 md:px-10 fixed bg-white w-full shadow items-center">
       <Link href={"/"} className="text-3xl font-extrabold  text-purple-950">
         <div className="relative w-64 h-7 my-2">
           <Image
             src={FullLogoImg}
             fill
             alt="multroic logo"
-            className="object-contain object-center"
+            className="object-contain object-left md:object-center"
           />
         </div>
       </Link>
-      <ul className="flex space-x-8 font-bold text-xl text-purple-950">
+
+      <div>
+        <GiHamburgerMenu size={25} className="text-purple-950" />
+      </div>
+
+      <ul className="hidden md:flex space-x-8 font-bold text-xl text-purple-950">
         <li>
           <Link href={"/"}>About</Link>
         </li>
