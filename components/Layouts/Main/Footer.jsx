55d14290883719b0156190ea88fc5ea53eea20ed
Hari Perisetla <harinagasai.perisetla@gmail.com>
2023-04-21 11:48:50 +0530

    footer mobile responsiveness added

--- components/Layouts/Main/Footer.jsx
+++ components/Layouts/Main/Footer.jsx
@@ -7,15 +7,15 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="text-center bg-purple-900 text-white">
-      <div className="py-20 container mx-auto space-x-20 max-w-6xl grid grid-cols-4">
-        <div className=" text-left">
-          <div className="relative w-64 h-7 mb-3">
+    <footer className="w-full bottom-0 bg-secondary text-white flex flex-col items-center justify-center pt-10 bg-purple-900">
+      <div className="grid grid-cols-1 md:grid-cols-4 max-w-6xl p-10 md:p-20 space-y-10 md:space-y-0 md:space-x-20">
+        <div className="text-center md:text-left w-full flex flex-col justify-center">
+          <div className="relative w-full md:w-64 h-7 mb-3">
             <Image
               src={FullLogoWhiteImg}
               fill
               alt="multroic logo"
-              className="object-contain object-left"
+              className="object-contain object-center md:object-left"
             />
           </div>
           <div className="font-medium">
@@ -58,7 +58,7 @@ const Footer = () => {
           </ul>
         </div>
       </div>
-      <div className="bg-purple-950 py-3 font-medium">
+      <div className="bg-purple-950 w-full text-center py-3 font-medium">
         &copy; {currentYear}{" "}
         <Link
           href={"/"}
