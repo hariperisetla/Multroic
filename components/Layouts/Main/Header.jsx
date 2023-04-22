94ecd7949867d575e0633ef58059c6c313e365dc
Hari Perisetla <harinagasai.perisetla@gmail.com>
2023-04-22 11:46:16 +0530

    menu items refactored and added get started button

--- components/Layouts/Main/Header.jsx
+++ components/Layouts/Main/Header.jsx
@@ -5,6 +5,8 @@ import { FullLogoImg } from "@/assets/img/Img";
 import { GiHamburgerMenu } from "@/assets/icons/index";
 
 const Header = () => {
+  const menu = ["about", "blog", "games", "developers", "contact"];
+
   return (
     <header className="z-20 flex justify-between py-3 px-5 md:px-10 fixed bg-white w-full shadow items-center">
       <Link href={"/"} className="text-3xl font-extrabold  text-purple-950">
@@ -18,25 +20,23 @@ const Header = () => {
         </div>
       </Link>
 
-      <div>
+      <div className="md:hidden flex">
         <GiHamburgerMenu size={25} className="text-purple-950" />
       </div>
 
       <ul className="hidden md:flex space-x-8 font-bold text-xl text-purple-950">
+        {menu.map((page) => (
+          <li key={page}>
+            <Link href={page} className="capitalize">
+              {page}
+            </Link>
+          </li>
+        ))}
+
         <li>
-          <Link href={"/"}>About</Link>
-        </li>
-        <li>
-          <Link href={"/"}>Games</Link>
-        </li>
-        <li>
-          <Link href={"/"}>Developers</Link>
-        </li>
-        <li>
-          <Link href={"/"}>Contact</Link>
-        </li>
-        <li>
-          <Link href={"/"}>Blog</Link>
+          <Link href={"/"} className="bg-purple-950 text-white p-3">
+            Get Started
+          </Link>
         </li>
       </ul>
     </header>
