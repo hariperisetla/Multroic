d0be687fed823e50d788f32061c2af4231684abf
Hari Perisetla <harinagasai.perisetla@gmail.com>
2023-05-07 14:06:34 +0530

    Add Contact and FAQ components with Contact Form functionality

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,7 +1,18 @@
+import Faqs from "@/components/Layouts/main/contact/Faqs";
+import ContactForm from "@/components/Layouts/main/contact/Form";
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="">
+      <div className="container mx-auto py-8 px-4">
+        <div className="grid grid-cols-2">
+          <ContactForm />
+          <Faqs />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Contact.layout = "MainL";
