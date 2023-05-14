59c91cb282d00d1b54508ab326113375f5b02f0f
Hari Perisetla <harinagasai.perisetla@gmail.com>
2023-05-14 22:46:31 +0530

    code refactored for games and comments added for index and games pages

--- pages/games.jsx
+++ pages/games.jsx
@@ -1,24 +1,35 @@
-import GameCard from "@/components/main/games/GameCard";
-import Image from "next/image";
+// Importing the 'React' and 'GameCard' components from their respective modules.
 import React from "react";
+import GameCard from "@/components/main/games/GameCard";
+
+// Importing the 'gameContent' data from a local JSON file.
 import gameContent from "../data/main/games.json";
 
+// Defining the 'Games' component as an arrow function.
 const Games = () => {
+  // Defining a helper function to map over the 'gameContent' data and render a 'GameCard' component for each item.
+  const renderGameCards = () => {
+    return gameContent.map((game) => {
+      return <GameCard key={game.id} game={game} />;
+    });
+  };
+
+  // The 'Games' component returns a container element with a heading and a grid of 'GameCard' components.
   return (
-    <div className="space-y-8 container mx-auto pb-10">
-      <h1 class="text-5xl font-extrabold bg-custom-gradient text-transparent bg-clip-text text-center">
+    <div className="container mx-auto pb-10 space-y-8">
+      <h1 className="text-5xl font-extrabold text-center text-transparent bg-clip-text bg-custom-gradient">
         Games
       </h1>
 
-      <div className="grid grid-cols-1 px-10 md:grid-cols-4 gap-10">
-        {gameContent.map((game) => (
-          <GameCard key={game.id} game={game} />
-        ))}
+      <div className="grid grid-cols-1 gap-10 px-10 md:grid-cols-4">
+        {renderGameCards()}
       </div>
     </div>
   );
 };
 
+// Assigning the 'MainL' layout to the 'Games' component.
 Games.layout = "MainL";
 
+// Exporting the 'Games' component as the default export of this module.
 export default Games;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+// Importing the 'Hero' and 'Features' components from the '@/components/main' module.
 import { Hero, Features } from "@/components/main";
 
+// Defining the 'Home' component as an arrow function.
 const Home = () => {
+  // The 'Home' component returns a fragment containing the 'Hero' and 'Features' components.
   return (
     <>
       <Hero />
@@ -9,6 +12,8 @@ const Home = () => {
   );
 };
 
+// Assigning the 'MainL' layout to the 'Home' component.
 Home.layout = "MainL";
 
+// Exporting the 'Home' component as the default export of this module.
 export default Home;
