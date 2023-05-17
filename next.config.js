7e6cef8f1e951f56618f13df949d59de471b1f6b
Hari Perisetla <harinagasai.perisetla@gmail.com>
2023-05-17 11:06:48 +0530

    randomuser.me added to next config for placeholder images developer page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        port: "",
+        pathname: "/api/**",
+      },
+    ],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
