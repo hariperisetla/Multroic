bf133cdba221427be508a0bf57f4f29436dd46d0
hariperisetla <harinagasai.perisetla@gmail.com>
2023-08-06 22:34:05 +0530

    Create Footer.jsx

---
+++ components/dashboard/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-zinc-800 py-4">
+      <div className="container mx-auto text-center">
+        <p className="text-zinc-400">
+          &copy; 2023 Multroic. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
